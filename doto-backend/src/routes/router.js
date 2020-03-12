b6882eb51951a679446a00bfb1a3448206746d18
In Ha Ryu <iryu815@aucklanduni.ac.nz>
2020-03-13 01:08:13 +1300

    Use Promises to Retrieve/Save from/to database which is natural to js

--- doto-backend/src/routes/router.js
+++ doto-backend/src/routes/router.js
@@ -4,35 +4,34 @@ const User = require('../models/User');
 
 // GET ALL users
 router.get('/users', function(req, res){
-    let users = User.find({}, function(err, users){
-        if(err){
-            console.log(err);
-            res.json({msg: "failed"})
-        }
-        else {
-            console.log(users)
-            res.json(users);
-        }
+    User.find({})
+    .then(users => {
+        console.log(users)
+        res.status(200).json(users)
+    })
+    .catch((err) => {
+        console.log(err)
+        res.status(500).json(err)
     })
 })
 
 // ADD user
-router.post('/users/add', function (req, res) {
+router.post('/users', function (req, res) {
 
     let user = new User();
     user.name = req.body.name;
     user.email = req.body.email;
     user.password = req.body.password;
 
-    user.save(function(err){
-        if(err){
-            console.log(err);
-            res.json({msg: "failed..."})
-        }
-        else{
-            res.json(user)
-        }
-    });
+    user.save()
+    .then(() => {
+        console.log(user)
+        res.status(200).json(user)
+    })
+    .catch((err) => {
+        console.log(err)
+        res.status(500).json(err)
+    })
 });
 
 module.exports = router;
