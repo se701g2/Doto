1b36cc6d350907dc59743498068c333b69514c91
In Ha Ryu <iryu815@aucklanduni.ac.nz>
2020-03-12 15:09:46 +1300

    fix typo

---
+++ doto-frontend/src/components/pages/Calendar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Calender = () => {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div>Calender</div>
+            </header>
+        </div>
+    );
+};
+
+export default Calender;
