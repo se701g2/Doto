55a33d1dff02641c88ea17b6122c882dfccee40b
Mario Sinovcic <38930019+MarioSinovcic@users.noreply.github.com>
2020-03-12 22:56:07 +1300

    Modal functionality added
    - still need to create the form

---
+++ doto-frontend/src/components/pages/ModalContent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ModalContent = () => {
+    return (
+        <div className="modal-content">
+            <div>First Name</div>
+        </div>
+    );
+};
+
+export default ModalContent;
