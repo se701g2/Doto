83e9d80a2f53eb9127e392dcfb7a8cb2fab7b584
Cvos0162 <cookys0162@hotmail.co.kr>
2020-03-12 22:27:08 +1300

    Enhancement/#67-Setup Router with sample pages (#68)
    * Setup Router with sample pages
    
    * fix typo
    
    * Add web config file for deploy (#53)
    
    fix typo
    
    Setup Router with sample pages
    
    fix typo
    
    Setup Router with sample pages
    
    * typo Calender -> Calendar
    
    * remove log file that is renamed by accident
    
    * renamed routes/common.js -> routes/Route.js
    
    * apply changes renaming common to route in index.js
    
    Co-authored-by: Casey Wong <38745455+cwon880@users.noreply.github.com>

---
+++ doto-frontend/src/components/pages/Settings.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Settings = () => {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <div>Settings</div>
+            </header>
+        </div>
+    );
+};
+
+export default Settings;
