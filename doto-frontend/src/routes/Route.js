2a05f8227e32cf80f482677afd5c44076edf5bd5
In Ha Ryu <iryu815@aucklanduni.ac.nz>
2020-03-15 20:23:58 +1300

    Create initial Route.test.js that tests Route.js

--- doto-frontend/src/routes/Route.js
+++ doto-frontend/src/routes/Route.js
@@ -1,21 +1,21 @@
 import React from "react";
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import App from "../App";
 import Settings from "../components/pages/Settings";
 import Calendar from "../components/pages/Calendar/CalendarPage";
 import Login from "../components/pages/Login/Login";
 import NotFound from "../components/pages/NotFound";
 
-export default (
-    <Router>
-        <div>
-            <Switch>
-                <Route exact path="/" component={App} />
-                <Route path="/settings" component={Settings} />
-                <Route path="/calendar" component={Calendar} />
-                <Route path="/login" component={Login} />
-                <Route component={NotFound} />
-            </Switch>
-        </div>
-    </Router>
-);
+const Routes = () => {
+    return (
+        <Switch>
+            <Route exact path="/" component={App} />
+            <Route path="/settings" component={Settings} />
+            <Route path="/calendar" component={Calendar} />
+            <Route path="/login" component={Login} />
+            <Route component={NotFound} />
+        </Switch>
+    );
+};
+
+export default Routes;
