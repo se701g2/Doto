cfdae8dddbdf01a12e13613c921d9d614dbe838d
emzoo16 <43309131+emzoo16@users.noreply.github.com>
2020-03-17 10:38:16 +1300

    match file to master

--- doto-frontend/src/FeatureFlag.test.js
+++ doto-frontend/src/FeatureFlag.test.js
@@ -1,14 +1,14 @@
 import * as configcat from "configcat-js";
 
-test("false feature flags are functional", async () => {
-    const configCatClient = configcat.createClient("9sPXCALhtpYo4pqg4g3vDQ/KtGlH7JdfUunUsbdCUwWaw");
+test('false feature flags are functional', async () => {
+    let configCatClient = configcat.createClient("9sPXCALhtpYo4pqg4g3vDQ/KtGlH7JdfUunUsbdCUwWaw");
     const isFeatureFlagFalse = await configCatClient.getValueAsync("featureflagfalse", false);
     console.log(isFeatureFlagFalse);
     expect(isFeatureFlagFalse).toBe(false);
 });
 
-test("true feature flags are functional", async () => {
-    const configCatClient = configcat.createClient("9sPXCALhtpYo4pqg4g3vDQ/KtGlH7JdfUunUsbdCUwWaw");
+test('true feature flags are functional', async () => {
+    let configCatClient = configcat.createClient("9sPXCALhtpYo4pqg4g3vDQ/KtGlH7JdfUunUsbdCUwWaw");
     const isFeatureFlagTrue = await configCatClient.getValueAsync("featureflagtrue", true);
     expect(isFeatureFlagTrue).toBe(true);
 });
