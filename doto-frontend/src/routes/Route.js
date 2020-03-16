28803a69314b9e957ea3d10a4e26080e4adf9876
Babra Ajaz <48302817+baja156@users.noreply.github.com>
2020-03-16 18:11:32 +1300

    Front end/#124 removing redundant files and creating necessary routes (#125)
    * Code of Conduct
    
    * Delete CODE_OF_CONDUCT
    
    * CODE_OF_CONDUCT
    
    The following document specifies the etiquette that should be following when using Taffy's OSS and also the following:
    Where the code of conduct takes effect?
    Whom the code of conduct applies to?
    What happens if someone violates the code of conduct?
    How someone can report violations?
    
    * Delete CODE OF CONDUCT
    
    * Creating settings route

--- doto-frontend/src/routes/Route.js
+++ doto-frontend/src/routes/Route.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import App from "../App";
-import Settings from "../components/pages/Settings";
+import SettingsPage from "../components/pages/Settings/SettingsPage";
 import Login from "../components/pages/Login/Login";
 import Calendar from "../components/pages/Calendar/Calendar";
 import NotFound from "../components/pages/NotFound";
@@ -11,7 +11,7 @@ export default (
         <div>
             <Switch>
                 <Route exact path="/" component={App} />
-                <Route path="/settings" component={Settings} />
+                <Route path="/settings" component={SettingsPage} />
                 <Route path="/calendar" component={Calendar} />
                 <Route path="/login" component={Login} />
                 <Route component={NotFound} />
