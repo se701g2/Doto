3c1e9c43b7aa07fe26c68c913438597d727942a2
emzoo16 <43309131+emzoo16@users.noreply.github.com>
2020-03-17 11:44:06 +1300

    Issue/#41 setup google oauth (#132)
    * Creating Task Schema
    
    * added tests
    
    * swapping out hardcoded value
    
    * set up google oauth
    
    * match file to master
    
    * Add default frontend redirect url
    
    Co-authored-by: Emily Zou <emilyzou@Mr.local>
    Co-authored-by: Casey Wong <cwong367@gmail.com>

---
+++ doto-backend/src/config/passport-setup.js
@@ -0,0 +1,28 @@
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20')
+const User = require('../models/User');
+
+passport.use(
+    new GoogleStrategy({
+        callbackURL: '/auth/google/redirect',
+        clientID: '181904764798-mbguideg0blo89p18ibhjkknh6e2chsd.apps.googleusercontent.com',
+        clientSecret: '73Tk75zlPqvruJmUAYdO1igD'
+    }, (accessToken, refreshToken, profile, email, done) => {
+        var user
+        //Check if user already exists in database
+        User.findOne({name: email._json.email}).then((currentUser)=>{
+            if(currentUser){
+                console.log('User already exists ' + newUser.name)
+                user = currentUser
+            }else{
+                user = new User({
+                    name: email._json.email
+                })
+                user.save().then((newUser)=>{
+                    console.log('Created New User ' + newUser.name)
+                })
+            }
+            done(null, user)
+        })
+    })
+)
