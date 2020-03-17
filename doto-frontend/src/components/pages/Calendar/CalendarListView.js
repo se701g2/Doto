84a28fbeecacd4623c8b5b8dba5bf389e6eecfd7
Dinith <34013038+Dinith1@users.noreply.github.com>
2020-03-18 09:10:16 +1300

    Feature/117-scheduling (#151)
    * created function skeleton
    
    * finished basic algorithm
    
    * fixed modal
    
    * completed EDF scheduler
    
    * fixed merge issue
    
    Co-authored-by: dinith1 <dinith1@github.com>

--- doto-frontend/src/components/pages/Calendar/CalendarListView.js
+++ doto-frontend/src/components/pages/Calendar/CalendarListView.js
@@ -3,19 +3,19 @@ import React from "react";
 import Radio from "@material-ui/core/Radio";
 import "./Calendar.css";
 
-const CalendarListView = ({ appointments }) => {
+const CalendarListView = ({ tasks }) => {
     return (
         <div className="list-view">
             <div className="ml-3 mb-5 text-4xl font-bold">Tasks for Today</div>
-            {appointments.map(appointment => {
+            {tasks.map(task => {
                 return (
                     <div key className="list-view-components">
                         <Radio
                             disableRipple
-                            checkedIcon={<span className={"radio-checked bg-" + appointment.color + "-600"} />}
+                            checkedIcon={<span className={"radio-checked bg-" + task.color + "-600"} />}
                             icon={<span className="radio-unchecked" />}
                         />
-                        <div className="text-base">{appointment.title}</div>
+                        <div className="text-base">{task.title}</div>
                     </div>
                 );
             })}
