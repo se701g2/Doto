c1c9b815126872bd6187511877e80b4112fe1633
Babra Ajaz <48302817+baja156@users.noreply.github.com>
2020-03-18 13:09:40 +1300

    Improving list view (#152)

--- doto-frontend/src/components/pages/Calendar/CalendarListView.js
+++ doto-frontend/src/components/pages/Calendar/CalendarListView.js
@@ -4,19 +4,32 @@ import Radio from "@material-ui/core/Radio";
 import "./Calendar.css";
 
 const CalendarListView = ({ tasks }) => {
+    const today = new Date();
+
     return (
         <div className="list-view">
             <div className="ml-3 mb-5 text-4xl font-bold">Tasks for Today</div>
             {tasks.map(task => {
+                const isTaskToday =
+                    task.dueDate.getYear() === today.getYear() && task.dueDate.getMonth() === today.getMonth()
+                        ? task.dueDate.getDate() === today.getDate()
+                        : false;
+
                 return (
-                    <div key className="list-view-components">
-                        <Radio
-                            disableRipple
-                            checkedIcon={<span className={"radio-checked bg-" + task.color + "-600"} />}
-                            icon={<span className="radio-unchecked" />}
-                        />
-                        <div className="text-base">{task.title}</div>
-                    </div>
+                    isTaskToday && (
+                        <div key className="list-view-components">
+                            <Radio
+                                disableRipple
+                                checkedIcon={
+                                    <span
+                                        className={"radio-checked bg-" + (task.color ? task.color : "blue") + "-600"}
+                                    />
+                                }
+                                icon={<span className="radio-unchecked" />}
+                            />
+                            <div className="text-base">{task.title}</div>
+                        </div>
+                    )
                 );
             })}
         </div>
