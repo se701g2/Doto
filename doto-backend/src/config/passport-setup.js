9532c80266b3042dec719f755d6052954b758a0a
allen <awan968@aucklanduni.ac.nz>
2020-03-20 08:43:55 +1300

    Persisted user info into user model upon login

--- doto-backend/src/config/passport-setup.js
+++ doto-backend/src/config/passport-setup.js
@@ -7,16 +7,19 @@ passport.use(
         callbackURL: '/auth/google/redirect',
         clientID: process.env.GOOGLE_API_CLIENT,
         clientSecret: process.env.GOOGLE_API_SECRET
-    }, (accessToken, refreshToken, profile, email, done) => { 
+    }, (accessToken, refreshToken, profile, done) => { 
+        const email = profile.emails[0].value
         var user
         //Check if user already exists in database
-        User.findOne({ email: email._json.email }).then((currentUser) => {
+        User.findOne({ email }).then((currentUser) => {
             if (currentUser) {
                 console.log('User already exists ' + currentUser.email)
                 user = currentUser
             } else {
                 user = new User({
-                    email: email._json.email,
+                    email,
+                    name: profile._json.name,
+                    picture: profile._json.picture
                 })
                 user.save().then((newUser) => {
                     console.log('Created New User ' + newUser.email)
