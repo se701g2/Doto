75b491a7de3b0ef6cf9b7308e0229790d18bf0d2
Jordan Sim-Smith <jordansimsmith@gmail.com>
2020-03-30 12:07:08 +1300

    issue/210 - Extend formatting toolchain to work with both projects (#217)
    * Remove husky from doto-frontend
    
    * Initialise multi package project with lerna
    
    * Simplify node modules installation with one command
    
    * Configure husky on root package
    
    * Configure doto-frontend package for precommit lint-staged
    
    * Configure doto-backend package for precommit lint-staged
    
    * Configure eslintrc for mocha testing
    
    * Format all doto-backend files
    
    * Reformat files after rebase
    
    * Update readme with lerna and yarn instructions

--- doto-backend/src/common/logging.js
+++ doto-backend/src/common/logging.js
@@ -1,14 +1,12 @@
-const {createLogger, transports, format} = require('winston');
+const { createLogger, transports, format } = require("winston");
 
 const logger = createLogger({
     transports: [new transports.Console()],
     format: format.combine(
         format.timestamp(),
         format.colorize(),
-        format.printf(
-            info => `${info.timestamp} [${info.level}] ${JSON.stringify(info.message)}`
-        )
+        format.printf((info) => `${info.timestamp} [${info.level}] ${JSON.stringify(info.message)}`),
     ),
-})
+});
 
 module.exports.logger = logger;
