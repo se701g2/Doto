667b6ea32d865381a938dd0d5323444a0299ce81
Jordan Sim-Smith <jordansimsmith@gmail.com>
2020-03-30 00:58:21 +1300

    Format all doto-backend files

--- doto-backend/index.js
+++ doto-backend/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config();
-const passportSetUp = require('./src/config/passport-setup.js')
+require('./src/config/passport-setup.js')
 const app = express()
 const apiPort = process.env.PORT || 3001
 const passport = require('passport')
@@ -10,15 +10,15 @@ const expressWinston = require('express-winston');
 const { logger } = require('./src/common/logging');
 
 // Mongoose connection
-const mongoose = require('mongoose');
-// db connection string will point to Azure string only in production, fallbacks to dev database string 
+const mongoose = require("mongoose");
+// db connection string will point to Azure string only in production, fallbacks to dev database string
 const connectionString = process.env.AZURE_CONN || process.env.DEVELOPMENT_DB_CONN;
 // Add authentication strings only in production environment
-const connParams = { useNewUrlParser: true }
-if(process.env.AZURE_USER && process.env.AZURE_PW){
+const connParams = { useNewUrlParser: true };
+if (process.env.AZURE_USER && process.env.AZURE_PW) {
     connParams.auth = {
-        user : process.env.AZURE_USER,
-        password: process.env.AZURE_PW
+        user: process.env.AZURE_USER,
+        password: process.env.AZURE_PW,
     };
 }
 mongoose.connect(connectionString, connParams);
@@ -52,17 +52,17 @@ app.use(cors())
 app.use(express.json())
 app.use(passport.initialize());
 
-// exporting Routes 
-const task = require('./src/routes/task-route');
-app.use('/task', task);
-const user = require('./src/routes/user-route');
-app.use('/user', user);
-const authRoute = require('./src/routes/auth-route')
-app.use('/auth', authRoute)
+// exporting Routes
+const task = require("./src/routes/task-route");
+app.use("/task", task);
+const user = require("./src/routes/user-route");
+app.use("/user", user);
+const authRoute = require("./src/routes/auth-route");
+app.use("/auth", authRoute);
 
-//Swagger UI Setup
-var swaggerUi = require('swagger-ui-express');
-swaggerDocument = require('./swagger.json');
-app.use('/',swaggerUi.serve,swaggerUi.setup(swaggerDocument));
+// Swagger UI Setup
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(apiPort, () => logger.info(`Server running on port ${apiPort}`))
--- doto-backend/src/config/passport-setup.js
+++ doto-backend/src/config/passport-setup.js
@@ -5,18 +5,20 @@ const { logger } = require('../common/logging');
 
 // This function applies the Google strategy to passport. Used in auth-route.
 passport.use(
-    new GoogleStrategy({
-        callbackURL: '/auth/google/redirect',   // User is directed here after successful login from Google login
+    new GoogleStrategy(
+        {
+            callbackURL: "/auth/google/redirect", // User is directed here after successful login from Google login
 
-        // Google API keys from dev account
-        clientID: process.env.GOOGLE_API_CLIENT,
-        clientSecret: process.env.GOOGLE_API_SECRET
+            // Google API keys from dev account
+            clientID: process.env.GOOGLE_API_CLIENT,
+            clientSecret: process.env.GOOGLE_API_SECRET,
 
-        // Callback function called after user login but before redirect
-        // Gets additional info about user from Google. In this case, profile info from Google.
-    }, (accessToken, refreshToken, profile, done) => {
-        const email = profile.emails[0].value
-        var user
+            // Callback function called after user login but before redirect
+            // Gets additional info about user from Google. In this case, profile info from Google.
+        },
+        (accessToken, refreshToken, profile, done) => {
+            const email = profile.emails[0].value;
+            var user;
 
         //Check if user already exists in database. Creates a new database entry if they don't exist.
         User.findOne({ email }).then((currentUser) => {
@@ -35,8 +37,9 @@ passport.use(
                 })
             }
 
-            // User Callback function complete, User is returned.
-            done(null, user)
-        })
-    })
-)
+                // User Callback function complete, User is returned.
+                done(null, user);
+            });
+        },
+    ),
+);
