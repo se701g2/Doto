11c4fa434827c2eac825be2dc2b1bc0e9e6f8ab0
Harrison Leach <44953072+HarrisonLeach1@users.noreply.github.com>
2020-03-31 19:24:27 +1300

    issue/213 - Add ability to complete a task (#223)
    * Add task completion on list view and calendar components
    
    Allows users to complete from either component and keeps data in sync
    
    * Update Appointment coloring when task is complete
    
    Provides clear indication to the user which of their tasks
    are complete and incomplete
    
    * Fix issue where tasks did not have keys
    
    Fixes task duplication issue
    
    * Add strikethrough when completing tasks
    
    * Remove unused radio styles
    
    * Update property name completed to isComplete
    
    * Connect frontend logic to backend
    
    This means the isComplete state is persisted in the backend
    
    * Add unit tests for updating isComplete

--- doto-frontend/src/components/pages/Calendar/CalendarComponent.js
+++ doto-frontend/src/components/pages/Calendar/CalendarComponent.js
@@ -14,10 +14,12 @@ import {
     Appointments,
     AppointmentTooltip,
 } from "@devexpress/dx-react-scheduler-material-ui";
+import { Checkbox, Grid } from "@material-ui/core";
+import DoneIcon from "@material-ui/icons/Done";
 
-const CalendarComponent = props => {
+const CalendarComponent = ({ tasks, onTaskStatusUpdated }) => {
     return (
-        <Scheduler data={props.tasks} currentView={MonthView} editable={true}>
+        <Scheduler data={tasks} currentView={MonthView} editable={true}>
             <ViewState />
             <DayView />
             <WeekView />
@@ -26,11 +28,49 @@ const CalendarComponent = props => {
             <ViewSwitcher />
             <DateNavigator />
             <TodayButton />
-            <Appointments />
-            <AppointmentTooltip />
+            <Appointments appointmentComponent={Appointment} />
+            <AppointmentTooltip
+                contentComponent={props => <Content {...props} onTaskStatusUpdated={onTaskStatusUpdated} />}
+            />
         </Scheduler>
     );
 };
+
+const Content = ({ children, appointmentData, style, onTaskStatusUpdated, ...restProps }) => {
+    return (
+        <AppointmentTooltip.Content {...restProps} appointmentData={appointmentData}>
+            <Grid container alignItems="center">
+                <Checkbox
+                    checked={appointmentData.isComplete}
+                    color="primary"
+                    onClick={() => onTaskStatusUpdated(appointmentData.taskId)}
+                />
+                <span>{appointmentData.isComplete ? "Task complete" : "Task incomplete"}</span>
+            </Grid>
+        </AppointmentTooltip.Content>
+    );
+};
+
+const Appointment = ({ children, style, ...restProps }) => (
+    <Appointments.Appointment
+        {...restProps}
+        style={
+            restProps.data.isComplete
+                ? { ...style, backgroundColor: "#adadad" }
+                : {
+                      style,
+                  }
+        }
+    >
+        {children}
+        <Grid container alignItems="center" justify="space-between">
+            <span style={{ color: "white", paddingLeft: "10px" }}>
+                {restProps.data.isComplete ? "Task complete" : "Task incomplete"}
+            </span>
+            {restProps.data.isComplete && <DoneIcon style={{ marginRight: "26px", fontSize: 26, color: "white" }} />}
+        </Grid>
+    </Appointments.Appointment>
+);
 
 CalendarComponent.propTypes = {
     tasks: PropTypes.array.isRequired,
