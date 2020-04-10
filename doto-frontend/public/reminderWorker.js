8ca64cd0f0d56170311e25e49d287788a01428e9
Tony Liu <ipwnu777@gmail.com>
2020-04-10 16:53:55 +1200

    Added start date to title of notification

--- doto-frontend/public/reminderWorker.js
+++ doto-frontend/public/reminderWorker.js
@@ -1,7 +1,17 @@
 self.addEventListener("push", ev => {
-    const data = ev.data.json();
-    self.registration.showNotification(data.title, {
-        body: data.description,
+    const { title, description, startDate: startDateIsoString } = ev.data.json();
+    const startDate = new Date(startDateIsoString);
+    let options = {
+        hour: "2-digit",
+        minute: "2-digit",
+    };
+    if (new Date().getDate() < startDate.getDate()) {
+        // Include more info if start date is on future date
+        options = { ...options, weekday: "long", month: "numeric", day: "numeric" };
+    }
+    const etaTitle = `${title} at ${startDate.toLocaleTimeString("en-nz", options)}`;
+    self.registration.showNotification(etaTitle, {
+        body: description,
         icon: "/favicon.ico",
     });
 });
