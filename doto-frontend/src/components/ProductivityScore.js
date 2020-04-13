d30e679972611c9c8a229467c71f350868d8e5ae
Utsav Trivedi <utsavtrivedi16@gmail.com>
2020-04-13 23:06:09 +1200

    issue/feature Add productivity mode slider for gamification system * Mode can be linked to points scored

---
+++ doto-frontend/src/components/ProductivityScore.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Slider from "@material-ui/core/Slider";
+import "./ProductivityScore.css";
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        height: 400,
+        width: 200,
+        paddingBottom: 30,
+        paddingTop: 30,
+    },
+    title: {
+        paddingLeft: 20,
+    },
+}));
+
+const ProductivityScore = props => {
+    const classes = useStyles();
+
+    const marks = [
+        {
+            value: 0,
+            label: "Lazy",
+        },
+        {
+            value: 10,
+            label: "Recovering Procrastinator",
+        },
+        {
+            value: 20,
+            label: "Caffeine Machine",
+        },
+        {
+            value: 30,
+            label: "Energy Drinkaholic",
+        },
+        {
+            value: 40,
+            label: "Workhorse God",
+        },
+    ];
+
+    return (
+        // Setting .css properties based on theme selected
+
+        <div>
+            <Typography id="discrete-slider-custom" gutterBottom className={classes.title}>
+                Productivity Mode
+            </Typography>
+            <div className={classes.root}>
+                <Slider
+                    defaultValue={10}
+                    // getAriaValueText={valuetext}
+                    orientation="vertical"
+                    aria-labelledby="discrete-slider-custom"
+                    step={10}
+                    max={40}
+                    // valueLabelDisplay="auto"
+                    marks={marks}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default ProductivityScore;
--- doto-frontend/src/components/pages/Calendar/Calendar.js
+++ doto-frontend/src/components/pages/Calendar/Calendar.js
@@ -7,8 +7,10 @@ import Fab from "@material-ui/core/Fab";
 import Tooltip from "@material-ui/core/Tooltip";
 import FormatListBulletedIcon from "@material-ui/icons/FormatListBulleted";
 import CalendarTodayIcon from "@material-ui/icons/CalendarToday";
+import ScoreIcon from "@material-ui/icons/Score";
 import AddIcon from "@material-ui/icons/Add";
 import ModalContent from "../../ModalContent";
+import ProductivityScore from "../../ProductivityScore";
 import CalendarComponent from "./CalendarComponent";
 import CalendarListView from "./CalendarListView";
 import Header from "../Header";
@@ -41,9 +43,18 @@ const useStyles = makeStyles(theme => ({
 const Calendar = () => {
     const classes = useStyles();
     const [listView, setListView] = useState();
+    const [isOpenScore, setIsOpenScore] = useState(false);
     const [tasks, setTasks] = useState([]);
     const [open, setOpen] = useState(false);
     const [theme, setTheme] = useContext(ThemeContext);
+
+    const handleIsScoreOpen = () => {
+        if (isOpenScore) {
+            setIsOpenScore(false);
+        } else {
+            setIsOpenScore(true);
+        }
+    };
 
     const handleOpen = () => {
         setOpen(true);
@@ -118,6 +129,13 @@ const Calendar = () => {
                         </Fab>
                     </Tooltip>
                 </div>
+                <div className="mb-3">
+                    <Tooltip title="Productivity Score View">
+                        <Fab onClick={handleIsScoreOpen} size="small">
+                            <ScoreIcon />
+                        </Fab>
+                    </Tooltip>
+                </div>
             </div>
             <span className="content-container">
                 <Header title="Calendar" />
@@ -147,6 +165,25 @@ const Calendar = () => {
                     <Fade in={open}>
                         <div className={classes.paper}>
                             <ModalContent addNewTask={addNewTask} modalBackground={theme} />
+                        </div>
+                    </Fade>
+                </Modal>
+                <Modal
+                    aria-labelledby="transition-modal-title"
+                    aria-describedby="transition-modal-description"
+                    className={classes.modal}
+                    open={isOpenScore}
+                    onClose={handleIsScoreOpen}
+                    closeAfterTransition
+                    BackdropComponent={Backdrop}
+                    BackdropProps={{
+                        timeout: 500,
+                    }}
+                >
+                    {/* Transition effects for list view of to-do tasks for today */}
+                    <Fade in={isOpenScore}>
+                        <div className={classes.paper}>
+                            <ProductivityScore />
                         </div>
                     </Fade>
                 </Modal>
